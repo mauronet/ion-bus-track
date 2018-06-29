c8a5f405055b615aab6732dfe6f42ccafe6861b3
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2018-06-28 20:55:00 -0500

    Initial commit

---
+++ src/pages/buscar-ruta/buscar-ruta.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buscar-ruta',
+  templateUrl: 'buscar-ruta.html'
+})
+export class BuscarRutaPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
---
+++ src/pages/mapa-ruta/mapa-ruta.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-mapa-ruta',
+  templateUrl: 'mapa-ruta.html'
+})
+export class MapaRutaPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
